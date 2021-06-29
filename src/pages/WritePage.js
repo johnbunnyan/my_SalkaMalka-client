d428220a32d416f28fa1980451a5ebf0274b4f96
whdid502 <whdid1234@naver.com>
2021-06-29 17:02:56 +0900

    writepage/writepage완료

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import SideBar from "../component/SideBar";
+import ImageUpload from "../component/ImageUpload";
 
 class WritePage extends Component {
   constructor(props) {
     super(props)
+    this.handleImage = this.handleImage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
   };
+
+  state = {
+    title: '',
+    content: '',
+    image: null
+  }
+
+  handleImage = (data) => {
+    this.setState({
+      image: data
+    })
+  }
+
+  handleSubmit = () => {
+    // axios전송
+    // console.log(this.state)
+  }
+
   render() {
     return (
-      <SideBar isSignIn={this.props.isSignIn} signIn={this.props.signIn} signOut={this.props.signOut} signUp={this.props.signUp}></SideBar>
+      <div className={'write-page'}>
+        <SideBar isSignIn={this.props.isSignIn} signIn={this.props.signIn} signOut={this.props.signOut} signUp={this.props.signUp}></SideBar>
+        <div className={'wp-content'}>
+          <input type={'text'} defaultValue={'제목을 입력하세요'} onChange={(e) => { this.setState({ title: e.target.value }) }}></input>
+          <input type={'text'} defaultValue={'내용을 입력하세요'} onChange={(e) => { this.setState({ content: e.target.value }) }}></input>
+          <ImageUpload handleImage={this.handleImage}></ImageUpload>
+          <button onClick={() => { this.handleSubmit() }}>작성</button>
+        </div>
+      </div>
     )
   }
 }
